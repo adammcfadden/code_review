02d0cb256da5f5d071db7111873e4218425e000d
Adam McFadden <mcfadden.113@gmail.com>
2015-04-24 09:36:03 -0700

    add a second spec to test programs ability to count one word within a string senctence.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -2,7 +2,11 @@ require 'rspec'
 require 'word_counter'
 
 describe('String#word_counter') do
-  it('receive a one word input to count for, receive the same word to count in. Count the correct number or words, one.') do
+  it('receive a one word input to count for, receive the same word to count in. Count the correct number o words, one.') do
     expect('world'.word_counter('world')).to(eq('1'))
+  endf
+
+  it('receive a one word input to count for, receive a sentence to count in. Count the corrent number of words.')
+    expect('red'.word_counter("The red dog walked down the red dirt street, into a red sunset")).to(eq('3'))
   end
 end
