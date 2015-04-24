5cfcfe0ecc72df2bfa40c64f853d9d78b9725c7a
Adam McFadden <mcfadden.113@gmail.com>
2015-04-24 10:05:04 -0700

    add an integration test to the app. Test that the path from forms to output is working correctly.

---
+++ spec/word_counter_integration_spec.rb
@@ -0,0 +1,13 @@
+require ('capybara/rspec')
+require ('./app')
+Capybara.app = Sinatra::Application
+
+describe('The word counter path', {:type => :feature}) do
+  it('receive sentence and word search input from the user, send those through the word_counter method and return the values on the counter page') do
+    visit('/')
+    fill_in('search_word', :with => 'blue')
+    file_in('sentence', :with => 'The Blue moon was rising up over bluetown and the blue boy picked some berries. His blue dog chased a blue squirrel to the "Blue Tavern" and came back with a pint.')
+    click_button('sumbit_search')
+    expect(page).to(have_content('5'))
+  end
+end
