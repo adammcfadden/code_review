d5f7dbf36f94ebb21ba2caafcc335fa279dab805
Adam McFadden <mcfadden.113@gmail.com>
2015-04-24 09:32:51 -0700

    fix syntax errors in word_counter_spec.rb and add code to word_counter.rb to allow spec to pass.

---
+++ lib/word_counter.rb
@@ -0,0 +1,9 @@
+class String
+  define_method(:word_counter) do |arg|
+
+    if self == arg
+      return "1"
+    end
+
+  end
+end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -3,6 +3,6 @@ require 'word_counter'
 
 describe('String#word_counter') do
   it('receive a one word input to count for, receive the same word to count in. Count the correct number or words, one.') do
-    expect('world'.word_converter('world')).it(.eq('1'))
+    expect('world'.word_counter('world')).to(eq('1'))
   end
 end
